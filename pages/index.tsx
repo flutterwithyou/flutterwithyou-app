9aa32571843dd36f0a9309e5d5c5fc3c6590a9bc
flutterwithyou <jeongjoo.front@korea.ac.kr>
2024-03-17 14:50:58 +0900

    init: 코드 작성

--- pages/index.tsx
+++ pages/index.tsx
@@ -17,8 +17,7 @@ export default function Home() {
       <main className={`${styles.main} ${inter.className}`}>
         <div className={styles.description}>
           <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>pages/index.tsx</code>
+            FlutterWithYou - 플러터 용어집
           </p>
           <div>
             <a
