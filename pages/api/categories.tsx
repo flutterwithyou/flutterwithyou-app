7ebf4b3cb5f6f32c74c99b191f12349ec4de06fa
Jiwon Kwon <kjiwon411@gmail.com>
2024-04-10 09:26:57 +0900

    refactor 전체 용어 불러오기, 전체 카테고리 불러오기 리팩토링

--- pages/api/categories.tsx
+++ pages/api/categories.tsx
@@ -1,18 +1,22 @@
-import clientPromise from "@/pages/lib/mongodb";
+import { MongoClient } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 
-export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+export default async function fetchCategoriesData (req: NextApiRequest, res: NextApiResponse) {
     try {
-        const client = await clientPromise;
+        const client = await MongoClient.connect('mongodb+srv://kjiwon411:HD4L97kJlB7OPWpU@cluster0.jlvxmnz.mongodb.net/fluttewithyou_flutterGlossaries?retryWrites=true&w=majority');
         const db = client.db("fluttewithyou_flutterGlossaries");
         const categories = await db
             .collection("categories")
             .find({})
             .toArray();
         res.json(categories);
-
         
+        client.close();
     } catch (e) {
         console.error(e);
     }
+    
+    return
+	
 }
--- pages/api/glossaries.tsx
+++ pages/api/glossaries.tsx
@@ -1,15 +1,18 @@
-import clientPromise from "@/pages/lib/mongodb";
+import { MongoClient } from 'mongodb';
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     try {
-        const client = await clientPromise;
+        const client = await MongoClient.connect('mongodb+srv://kjiwon411:HD4L97kJlB7OPWpU@cluster0.jlvxmnz.mongodb.net/fluttewithyou_flutterGlossaries?retryWrites=true&w=majority');
         const db = client.db("fluttewithyou_flutterGlossaries");
         const glossaries = await db
             .collection("glossaries")
             .find({})
             .toArray();
         res.json(glossaries);
+
+        client.close();
+
     } catch (e) {
         console.error(e);
     }
