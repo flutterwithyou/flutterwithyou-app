80ec52afb9565f5d33451ac0d3dfc3c69a3dcd60
Jiwon Kwon <kjiwon411@gmail.com>
2024-04-10 19:47:50 +0900

    refactor 전체 용어 조회 api 주석 추가

--- pages/api/glossaries/all.tsx
+++ pages/api/glossaries/all.tsx
@@ -8,22 +8,18 @@ import { NextApiRequest, NextApiResponse } from 'next';
 export default async function handler (req: NextApiRequest, res: NextApiResponse) {
    let dict = new Map<string, Glossary[]> ();
     try {
-        
+        // 데이터베이스에서 전체 용어 데이터 가져오기
        if (req.method == 'GET') {  const client = await MongoClient.connect('mongodb+srv://kjiwon411:HD4L97kJlB7OPWpU@cluster0.jlvxmnz.mongodb.net/fluttewithyou_flutterGlossaries?retryWrites=true&w=majority');
         const db = client.db("fluttewithyou_flutterGlossaries");
         const data = await db
             .collection("glossaries")
             .find({})
             .toArray();
-        // res.status(200).json(data);
+
 
         client.close();
         
-        /*glossaries = data.map((glossaries) => {
-                _id: glossaries._id.toString()
-            });
-        }
-        */
+    // 정의한 용어 타입 배열로 데이터 저장
         let glossaries: Glossary[] = data.map((doc) => ({
             id: doc._id.toString(),
             koreanName: doc.koreanName.toString(),
@@ -34,25 +30,27 @@ export default async function handler (req: NextApiRequest, res: NextApiResponse
             relationships: doc.relationships.toString()
         }));
         
-
+    // 용어 배열에 있는 용어의 첫번째 초성을 기준으로 저장
     for (const elem of glossaries) {
         
         const glossary = elem.koreanName;
-        const initialChar: string = await findInitialChar(glossary);
-        if (dict.has(initialChar)) {
+        const initialChar: string = await findInitialChar(glossary); // 첫번째 자음을 가져온다
+        if (dict.has(initialChar)) { // 기존 딕셔너리에 자음 키 값 존재 시 용어 추가
             dict.get(initialChar)?.push(elem);
         } else {
-            dict.set(initialChar, [elem]);
+            dict.set(initialChar, [elem]); // 없을 시 새로운 키 값과 용어 추가
         }
     }
 
+    // JSON 객체로 변환
     const jsonObject: { [key: string]: any } = {};
     dict.forEach((value, key) => {
         jsonObject[key] = value;
     });
 
+    // 상태 코드 200이면 JSON 객체 반환
     res.status(200).json(jsonObject);
-    console.log(jsonObject);
+    // console.log(jsonObject);
 
     }
      else {
