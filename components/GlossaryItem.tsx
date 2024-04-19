30c6d78062bc71c634e12e61853c8adaf1ff8b9f
devYoonji <103026665+devYoonji@users.noreply.github.com>
2024-04-19 16:34:48 +0900

    feat api연결 및 navbutton기능 추가

--- components/GlossaryItem.tsx
+++ components/GlossaryItem.tsx
@@ -1,42 +1,39 @@
-import React, { useState, useEffect } from 'react';
 import styles from "../styles/GlossaryPage.module.css";
 
-async function getData(url: string): Promise<any> {
-    const res = await fetch(url)
-
-    if (!res.ok) {
-        throw new Error('Failed to fetch data')
-    }
-
-    return res.json();
+interface Glossary {
+    id: string;
+    koreanName: string;
+    englishName: string;
+    category: string;
+    definition: string;
+    example: string;
+    relationships: string;
 }
 
-const GlossaryItem: React.FC = () => {
-    const [glossaryList, setGlossaryList] = useState<any[]>([]);
+interface GlossaryData {
+    [label: string]: Glossary[];
+}
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                let data = await getData('https://25547bcc-a8a7-412f-badd-977ce6c1732f.mock.pstmn.io/api/glossary');
-                const duplicatedData = Array.from({ length: 5 }, () => JSON.parse(JSON.stringify(data)));
-                data = data.concat(...duplicatedData);
-                setGlossaryList(data);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
+interface Props {
+    glossaryData: GlossaryData;
+}
 
-        fetchData();
-    }, []);
 
+const GlossaryItem: React.FC<Props> = ({ glossaryData }) => {
     return (
         <div className={styles.container}>
-            {glossaryList.map((glossary, index) => (
-                <div key={index} className={styles.glossary}>
-                    <div className={styles.row}>
-                    <h3>{glossary.koreanName} {glossary.englishName}</h3> <span className={styles.category}>{glossary.category}</span>
-                    </div>
-                    <p>{glossary.definition}</p>
+            {Object.entries(glossaryData).map(([label, glossaries]) => (
+                <div key={label}>
+                    <span className={styles.label} id={label}>{label}</span>
+                    {glossaries.map((glossary) => (
+                        <div key={glossary.id} className={styles.glossary}>
+                            <div className={styles.row}>
+                                <h3>{glossary.koreanName} {glossary.englishName}</h3>
+                                <span className={styles.category}>{glossary.category}</span>
+                            </div>
+                            <p>{glossary.definition}</p>
+                        </div>
+                    ))}
                 </div>
             ))}
         </div>
--- components/NavButton.tsx
+++ components/NavButton.tsx
@@ -3,30 +3,26 @@
 import React from 'react';
 import styles from "../styles/GlossaryPage.module.css";
 
-interface AlphabetNavigationProps {
-    onItemClick: (alphabet: string) => void;
-}
-
-const NavButton: React.FC<AlphabetNavigationProps> = ({ onItemClick }) => {
+const NavButton: React.FC<any> = ({ onItemClick, labels }) => {
 
     //todo: glossary가 있는 알파벳만 나열하기
 
-const numberAlphabets: string[] = Array.from({ length: 9 }, (_, index) => (index + 1).toString());
-const koreanAlphabets: string[] = ['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
-const englishAlphabets: string[] = Array.from({ length: 26 }, (_, index) => String.fromCharCode(65 + index));
+// const numberAlphabets: string[] = Array.from({ length: 9 }, (_, index) => (index + 1).toString());
+// const koreanAlphabets: string[] = ['ㄱ', 'ㄴ', 'ㄷ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅅ', 'ㅇ', 'ㅈ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
+// const englishAlphabets: string[] = Array.from({ length: 26 }, (_, index) => String.fromCharCode(65 + index));
 
-const alphabets: string[] = koreanAlphabets.concat(numberAlphabets, englishAlphabets);
+// const alphabets: string[] = koreanAlphabets.concat(numberAlphabets, englishAlphabets);
 
-const numColumns = 4; 
-const itemsPerColumn = Math.ceil(alphabets.length / numColumns);
+const numColumns = 1; 
+const itemsPerColumn = Math.ceil(labels.length / numColumns);
 
 return (
     <div className={styles.navigationContainer}>
         {[...Array(numColumns)].map((_, columnIndex) => (
             <div key={columnIndex} className={styles.column}>
-                {alphabets.slice(columnIndex * itemsPerColumn, (columnIndex + 1) * itemsPerColumn).map((alphabet, index) => (
-                    <button className={styles.navigation} key={index} onClick={() => onItemClick(alphabet)}>
-                        {alphabet}
+                {labels.slice(columnIndex * itemsPerColumn, (columnIndex + 1) * itemsPerColumn).map((label: string) => (
+                    <button className={styles.navigation} key={label} onClick={() => onItemClick(label)}>
+                        {label}
                     </button>
                 ))}
             </div>
