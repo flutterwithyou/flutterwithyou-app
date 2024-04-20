b6972760920d8c62a26146235e1ac085d68693aa
Jiwon Kwon <kjiwon411@gmail.com>
2024-04-21 03:17:29 +0900

    feat(): Swagger API 명세서 구현
    /api-doc 으로 접근

--- pages/api/glossaries/category/[category]/index.tsx
+++ pages/api/glossaries/category/[category]/index.tsx
@@ -1,3 +1,24 @@
+/**
+ * @swagger
+ * /api/glossaries/category/{category}:
+ *   get:
+ *     description: 카테고리에 속한 모든 용어들을 반환합니다.
+ *     parameters:
+ *      - in: path
+ *        name: category
+ *        schema:
+ *         type: string
+ *        required: true
+ *        description: 한글 카테고리 첫 단어의 영문명. e.g. 플러터&다트 -> flutter
+ *     responses:
+ *       200:
+ *         description: 용어들 중 해당 카테고리 프로퍼티를 가진 모든 용어들을 반환합니다.
+ *       405:
+ *          description: 잘못된 방법으로 접근
+ *       500: 
+ *          description: 내부 서버 에러 발생으로 인한 접근 거부
+ */
+
 import { MongoClient } from "mongodb";
 import { NextApiRequest, NextApiResponse } from 'next';
 
