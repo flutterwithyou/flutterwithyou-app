76bf894f3ead8ebbbd6ac424ca58842e8713aa8e
Jiwon Kwon <kjiwon411@gmail.com>
2024-04-25 00:05:23 +0900

    Update packages

--- pages/glossary.tsx
+++ pages/glossary.tsx
@@ -19,6 +19,7 @@ const GlossaryPage: NextPage = () => {
     const [glossaryData, setGlossaryData] = useState<any>({});
     const [labels, setLabels] = useState<string[]>([]);
     const glossaryRef = useRef<HTMLDivElement | null>(null);
+
     useEffect(() => {
         const fetchData = async () => {
             try {
@@ -32,6 +33,7 @@ const GlossaryPage: NextPage = () => {
         };
 
         fetchData();
+        
     }, []);
 
     const handleItemClick = (alphabet: string) => {
